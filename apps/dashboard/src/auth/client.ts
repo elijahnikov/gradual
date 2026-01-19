b97a53b054fa7e68715729025998b084fc8c60aa
elijahnikov <enkv@proton.me>
2026-01-19 15:27:28 +0000

    GRA-30: last used plugin

--- apps/dashboard/src/auth/client.ts
+++ apps/dashboard/src/auth/client.ts
@@ -1,7 +1,16 @@
 import { polarClient } from "@polar-sh/better-auth/client";
-import { emailOTPClient, organizationClient } from "better-auth/client/plugins";
+import {
+  emailOTPClient,
+  lastLoginMethodClient,
+  organizationClient,
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  plugins: [polarClient(), emailOTPClient(), organizationClient()],
+  plugins: [
+    polarClient(),
+    emailOTPClient(),
+    organizationClient(),
+    lastLoginMethodClient(),
+  ],
 });
